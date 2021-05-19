beef60ac7962bf7966b06d7c055318fe3ca61190
Gabo <chentevilo2001@gmail.com>
2021-05-19 11:36:58 -0600

    taller retrofit completo

--- app/src/main/java/com/example/retrofitlab/MainActivity.kt
+++ app/src/main/java/com/example/retrofitlab/MainActivity.kt
@@ -21,13 +21,29 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
     private lateinit var adapter:ArticleAdapter
     private val articleList = mutableListOf<Articles>()
 
+    private var pais_elegido: String ="us"
+    private var categoria: String ="general"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.searchNews.setOnQueryTextListener(this)
         initRecyclerView()
-        searchNew("business")
+        searchNew(pais_elegido,categoria)
+        binding.btnAr.setOnClickListener{ pais_elegido = "ar"
+            searchNew(pais_elegido,categoria) }
+        binding.btnUa.setOnClickListener{ pais_elegido = "ua"
+            searchNew(pais_elegido,categoria)}
+        binding.btnMx.setOnClickListener{ pais_elegido = "mx"
+            searchNew(pais_elegido,categoria)}
+        binding.btnBusiness.setOnClickListener{ categoria = "business"
+            searchNew(pais_elegido,categoria)}
+        binding.btnGeneral.setOnClickListener{ categoria = "general"
+            searchNew(pais_elegido,categoria)}
+        binding.btnHealth.setOnClickListener{ categoria = "health"
+            searchNew(pais_elegido,categoria)}
+
     }
 
     private fun initRecyclerView(){
@@ -35,10 +51,10 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
         binding.rvNews.layoutManager = LinearLayoutManager(this)
         binding.rvNews.adapter = adapter
     }
-    private fun searchNew(category: String){
+    private fun searchNew(pais: String,category: String){
         val api = Retrofit2()
         CoroutineScope(Dispatchers.IO).launch {
-            val call = api.getService()?.getNewsByCategory("us",category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
+            val call = api.getService()?.getNewsByCategory(pais,category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
             val news: NewsResponse? = call?.body()
 
             runOnUiThread{
@@ -57,9 +73,9 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
                 }
                 hideKeyBoard()
 
-                }
             }
         }
+    }
     private fun hideKeyBoard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(binding.viewRoot.windowToken, 0)
@@ -67,7 +83,7 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener{
     override fun onQueryTextSubmit(query: String?): Boolean {
         showMessage(query.toString())
         if (!query.isNullOrEmpty()){
-            searchNew(query.toLowerCase(Locale.ROOT))
+            searchNew(pais_elegido,query.toLowerCase(Locale.ROOT))
         }
         return true
     }
